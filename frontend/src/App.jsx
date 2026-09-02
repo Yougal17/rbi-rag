6521186046ea3c0bb1b822e97e2f5cb28657dc8f
Yougal17 <yougalattri17@gmail.com>
2026-09-02 18:35:15 +0530

    Updating cloudflare url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000" || "https://rbi-proxy.yougalattri17.workers.dev";
 
 function App() {
   const [question, setQuestion] = useState("");
